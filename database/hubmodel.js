d89600ba199ff96a3ebfc64ddc52ae99d40898c5
corruptionyt101-lab <corruptionyt101@gmail.com>
2026-09-19 15:51:54 -0400

    Create hubmodel.js

---
+++ database/hubmodel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true, maxlength: 40 },
+    icon: { type: String, default: "fa-gamepad" },
+    color: { type: String, default: "#0088ff" },
+    creator: { type: String, required: true }, // email of whoever made it
+    members: { type: [String], default: [] }, // usernames
+    views: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Hub", schema);
